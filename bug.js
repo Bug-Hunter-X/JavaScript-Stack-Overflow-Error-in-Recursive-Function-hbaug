541a2d76c36bf368c6cb8f95ec228a178441b4f0
BugHunter-X <askzenai@gmail.com>
2025-02-02 04:36:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+function foo(a, b) {
+  if (a === b) {
+    return true;
+  } else if (a > b) {
+    return false;
+  }
+  return foo(a + 1, b);
+}
+
+console.log(foo(1, 1)); // true
+console.log(foo(1, 5)); // Stack overflow error!
